47b4f041b459273a12c4383b2e3d5deb7981d52d
Glitch (gamblot) <none>
2019-09-20 05:20:22 +0000

    Add a very simple framework for blackjack (not active)

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ const Index = ()=> {
       <h1>Gamblot.</h1>
       <img className="storyImage" src="https://cdn.glitch.com/4cf04a5a-1809-4711-9bfe-3594a1152bfd%2Fpoker_1332601.png?v=1568067192852"/>
       <p>Gamblot is a gambling slack bot in the <a href="https://hackclub.com/community/" style={{textDecoration:"none",color:"red"}}>Hack Club</a> Slack workspace. This will basically make you either lose the money given, give it back, or give it back with a 200% multiplier! Now it even has a 1% chance for a 4x multiplier! To activate it type <code className="code">@banker give @Gamblot [amount]gp for [reason... idk]</code></p>
+      <iframe src="https://airtable.com/embed/shrsOsFBwZoUfbDGv?backgroundColor=gray" width="100%" height="533" style={{backgroundColor:"transparent", border:"1px solid #ccc"}}></iframe>
     </Layout>
   )
 }
